56dfa41316718902f426a27ab6f40bba801be5f5
Juulia <jusantal@cisco.com>
2020-05-29 14:42:16 +0300

    solution files exercise 1 and 2

---
+++ 2/exercise2_module_solution.py
@@ -0,0 +1,7 @@
+# an example solution for exercise 2, session 2
+# Module for credentials
+
+HOST = "ios-xe-mgmt-latest.cisco.com"
+USERNAME = "developer"
+PASSWORD = "C1sco12345"
+SECRET = "C1sco12345"
