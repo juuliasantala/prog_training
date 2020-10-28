b939cf5e7ea64291e19a85020a35de5ef1b7ab1f
Juulia <jusantal@cisco.com>
2020-10-28 14:27:36 +0200

    Delete exercise2_module_solution.py

--- 2/exercise2_module_solution.py
+++
@@ -1,7 +0,0 @@
-# an example solution for exercise 2, session 2
-# Module for credentials
-
-HOST = "ios-xe-mgmt-latest.cisco.com"
-USERNAME = "developer"
-PASSWORD = "C1sco12345"
-SECRET = "C1sco12345"
