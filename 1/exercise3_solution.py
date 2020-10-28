a05860e58db7816be9f280175660c6eddef0626a
Juulia <jusantal@cisco.com>
2020-10-28 14:23:56 +0200

    Delete exercise3_solution.py

--- 1/exercise3_solution.py
+++
@@ -1,7 +0,0 @@
-# an example solution for exercise 3, session 1
-
-print("Welcome! This application lets you calculate the sum of two numbers.")
-number1 = int(input("Enter the first number: "))
-number2 = int(input("Enter the second number: "))
-
-print("The sum of numbers {} and {} is {}".format(number1, number2, number1+number2))
